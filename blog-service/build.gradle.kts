1010dc02c6d26a182c71322b79f011e0db248977
xiaowansheng <xiaowansheng@foxmail.com>
2025-12-19 21:50:18 +0800

    feat: 升级依赖，修复gradle

--- blog-service/build.gradle.kts
+++ blog-service/build.gradle.kts
@@ -1,8 +1,8 @@
 plugins {
     java
-    kotlin("jvm") version "1.9.20"
-    id("org.springframework.boot") version "3.2.0"
-    id("io.spring.dependency-management") version "1.1.4"
+    kotlin("jvm") version "2.0.21"
+    id("org.springframework.boot") version "3.4.0"
+    id("io.spring.dependency-management") version "1.1.6"
 }
 
 group = "com.blog"
@@ -24,25 +24,25 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-mail")
     implementation("org.springframework.boot:spring-boot-starter-amqp")
     
-    implementation("com.baomidou:mybatis-plus-boot-starter:3.5.5")
-    implementation("com.baomidou:mybatis-plus-generator:3.5.5")
+    implementation("com.baomidou:mybatis-plus-boot-starter:3.5.7")
+    implementation("com.baomidou:mybatis-plus-generator:3.5.7")
     
-    implementation("cn.dev33:sa-token-spring-boot3-starter:1.37.0")
-    implementation("cn.dev33:sa-token-redis-jackson:1.37.0")
+    implementation("cn.dev33:sa-token-spring-boot3-starter:1.38.0")
+    implementation("cn.dev33:sa-token-redis-jackson:1.38.0")
     
     implementation("org.springframework.boot:spring-boot-starter-data-elasticsearch")
     
-    implementation("org.quartz-scheduler:quartz:2.3.2")
+    implementation("org.quartz-scheduler:quartz:2.3.3")
     
     implementation("io.springfox:springfox-boot-starter:3.0.0")
     
     implementation("org.projectlombok:lombok")
     annotationProcessor("org.projectlombok:lombok")
     
-    implementation("com.mysql:mysql-connector-j:8.2.0")
+    implementation("com.mysql:mysql-connector-j:9.1.0")
     
-    implementation("org.apache.commons:commons-lang3:3.14.0")
-    implementation("cn.hutool:hutool-all:5.8.23")
+    implementation("org.apache.commons:commons-lang3:3.17.0")
+    implementation("cn.hutool:hutool-all:5.8.28")
     
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
