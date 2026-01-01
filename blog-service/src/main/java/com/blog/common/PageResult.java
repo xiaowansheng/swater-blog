3979c822b33c65fb0785263c6c15bc096567c924
xiaowansheng <xiaowansheng@foxmail.com>
2026-01-01 17:32:47 +0800

    refactor(PageResult): 添加无参和全参构造函数
    为PageResult类添加Lombok的@NoArgsConstructor和@AllArgsConstructor注解，简化代码并提高可维护性

--- blog-service/src/main/java/com/blog/common/PageResult.java
+++ blog-service/src/main/java/com/blog/common/PageResult.java
@@ -1,9 +1,13 @@
 package com.blog.common;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import java.util.List;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class PageResult<T> {
     private List<T> records;
     private Long total;
